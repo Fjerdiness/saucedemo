5b449e6925f7555ec55e39bf6cd79e4bed232b5d
Fjerdiness <vocable_courts.05@icloud.com>
2024-10-20 15:06:56 +0100

    get_credentials refactoring

--- helpers/base_settings.py
+++ helpers/base_settings.py
@@ -1,11 +1,33 @@
 
 from typing import Tuple
 
-def get_credentials(user_type: str)  -> Tuple[str, str]:
+def get_credentials(user_type: str) -> Tuple[str, str]:
+    """
+    Retrieve the credentials for a specified user type.
+
+    This function looks up the password associated with the given user type
+    in the NON_VALID_CREDS dictionary. If a valid password is found, it returns
+    a tuple containing the user type and the corresponding password. If no 
+    valid password exists for the user type, it raises a ValueError.
+
+    Args:
+        user_type (str): The type of user for which to retrieve credentials.
+
+    Returns:
+            Tuple[str, str]: A tuple containing the user type and password.
+
+    Raises:
+        ValueError: If no valid credentials are found for the given user type.
+
+    Note:
+        NON_VALID_CREDS is expected to be a dictionary mapping user types to passwords.
+    """
     password = NON_VALID_CREDS.get(user_type)
     if password is not None:
         return user_type, password  
-    return None, None
+    else:
+        raise ValueError(f"No valid credentials found for user type: {user_type}")
+
 
 
 BASE_URL = "https://www.saucedemo.com/"
